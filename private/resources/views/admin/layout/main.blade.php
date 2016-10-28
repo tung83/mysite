fcb85c3c7b707f36060a96a4ea1eeabe3130db6a
unknown <TungNg@VNDEV18.augensoftwaregroup.com.vn>
2016-10-28 10:20:13 +0700

    implement logout

--- private/resources/views/admin/layout/main.blade.php
+++ private/resources/views/admin/layout/main.blade.php
@@ -244,41 +244,16 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">Alexander Pierce</span>
+              <i class="ion ion-person"></i>
+              <span class="hidden-xs">Admin</span>
             </a>
             <ul class="dropdown-menu">
-              <!-- User image -->
-              <li class="user-header">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
-                <p>
-                  Alexander Pierce - Web Developer
-                  <small>Member since Nov. 2012</small>
-                </p>
-              </li>
-              <!-- Menu Body -->
-              <li class="user-body">
-                <div class="row">
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Followers</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Sales</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Friends</a>
-                  </div>
-                </div>
-                <!-- /.row -->
-              </li>
+             
               <!-- Menu Footer-->
               <li class="user-footer">
-                <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
-                </div>
                 <div class="pull-right">
-                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                    <a id="logout" href="{{route('logout')}}" class="btn btn-default btn-flat"><i class="fa fa-sign-out"></i><span>Sign out</span></a>
+                    {!! Form::open(['url' => route('logout'), 'id' => 'logout-form']) !!}{!! Form::close() !!}
                 </div>
               </li>
             </ul>
@@ -533,6 +508,21 @@
 {!! HTML::script('admin/dist/js/app.min.js') !!}
 <!-- AdminLTE for demo purposes -->
 {!! HTML::script('admin/dist/js/demo.js') !!}
+<script>
+
+            $(function() {
+                $('#logout').click(function(e) {
+                    e.preventDefault();
+                    $('#logout-form').submit();
+                });
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+            });
+
+        </script>
 @stack('scripts')
 </body>
 </html>
